bca33464979d1941406c995dd34ffe2cb7ae1f27
Adam Giergun <adasss@poczta.onet.pl>
2022-08-04 13:51:14 +0200

    Fix parsing dailymotion json

--- app/src/main/java/eu/adamgiergun/lsi/network/dto/DailyMotionUsersPagesDTO.kt
+++ app/src/main/java/eu/adamgiergun/lsi/network/dto/DailyMotionUsersPagesDTO.kt
@@ -5,35 +5,27 @@ import com.squareup.moshi.JsonClass
 import eu.adamgiergun.lsi.users.data.local.UserDB
 
 @JsonClass(generateAdapter = true)
-data class DailyMotionUsersPagesDTO (
-    val pages: List<PageDTO>
-        ){
+data class DailyMotionUsersPagesDTO(
+    val list: List<DailyMotionUserDTO>
+) {
     @JsonClass(generateAdapter = true)
-    data class PageDTO(
-        val id: Int,
-        val dailymotionUsersDTO: List<DailyMotionUserDTO>
-    ) {
-        @JsonClass(generateAdapter = true)
-        data class DailyMotionUserDTO(
-            val id: String,
-            @Json(name = "screenname") val screenName: String
-        )
-    }
+    data class DailyMotionUserDTO(
+        val id: String,
+        @Json(name = "screenname") val screenName: String
+    )
 }
 
 fun DailyMotionUsersPagesDTO.asDbModel(): MutableList<UserDB> {
     val users = mutableListOf<UserDB>()
-    pages.forEach { page ->
-        page.dailymotionUsersDTO.forEach { userDTO ->
-            users.add(
-                UserDB(
-                    userDTO.id,
-                    userDTO.screenName,
-                    "",
-                    "Dailymotion"
-                )
+    list.forEach { userDTO ->
+        users.add(
+            UserDB(
+                userDTO.id,
+                userDTO.screenName,
+                "",
+                "Dailymotion"
             )
-        }
+        )
     }
     return users
 }
